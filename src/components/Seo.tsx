8538a18d8af73e6455c6c7a98b544fc820265e1d
Surya <git@sdan.io>
2022-10-16 17:56:00 -0400

    feat: seo fix

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -15,7 +15,7 @@ const defaultMeta = {
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-  image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
+  image: 'https://tanager.app/images/large-og.png',
 };
 
 type SeoProps = {
