2a9ecfd837e28c6e015ff586836c8ffe1563b031
Surya <git@sdan.io>
2022-10-30 05:02:14 -0400

    feat: fix quote tweet layout and bugs

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
 import { signIn, signOut, useSession, getSession } from 'next-auth/react';
+import Link from 'next/link';
 const unsigned_links = [{ href: '/', label: 'Sign in' }];
 const signed_links = [
   { href: '/', label: 'Top liked tweets' },
@@ -14,29 +15,28 @@ export default function Header() {
   // Design a header that follows the current design that shows a sign out button if the user is signed in, and a sign in button if the user is signed out.
   // Include a link to Top liked tweets and a link to Top liked quote tweets.
   // The styling should be similar to the current design.
-
-  return (
-    <header className='bg-blue-100'>
-      <div className='container mx-auto px-4'>
-        <nav className='flex items-center justify-between py-4'>
-          <div className='flex items-center'>
-            <UnstyledLink href='/'>
-              <a className='text-2xl font-bold text-gray-800'>Tanager</a>
-            </UnstyledLink>
-          </div>
-          <div className='flex items-center'>
-            {session ? (
+  if (session) {
+    return (
+      <header className='bg-blue-100'>
+        <div className='container mx-auto px-4'>
+          <nav className='flex items-center justify-between py-4'>
+            <div className='flex items-center'>
+              <Link href='/'>
+                <a className='text-2xl font-bold text-gray-800'>Tanager</a>
+              </Link>
+            </div>
+            <div className='flex items-center'>
               <>
-                <UnstyledLink href='/top_liked'>
+                <Link href='/top_liked'>
                   <a className='ml-4 text-gray-800 hover:text-gray-900 hover:underline'>
                     Top liked tweets
                   </a>
-                </UnstyledLink>
-                <UnstyledLink href='/quote_tweets'>
+                </Link>
+                <Link href='/quote_tweets'>
                   <a className='ml-4 text-gray-800 hover:text-gray-900 hover:underline'>
                     Top liked quote tweets
                   </a>
-                </UnstyledLink>
+                </Link>
                 <button
                   onClick={() => signOut()}
                   className='ml-4 text-gray-800 hover:text-gray-900 hover:underline'
@@ -44,16 +44,32 @@ export default function Header() {
                   Sign out
                 </button>
               </>
-            ) : (
-              <UnstyledLink href='/api/auth/signin'>
-                <a className='text-gray-800 hover:text-gray-900 hover:underline'>
-                  Sign in
-                </a>
-              </UnstyledLink>
-            )}
-          </div>
-        </nav>
-      </div>
-    </header>
-  );
+            </div>
+          </nav>
+        </div>
+      </header>
+    );
+  } else {
+    return (
+      <header className='bg-blue-100'>
+        <div className='container mx-auto px-4'>
+          <nav className='flex items-center justify-between py-4'>
+            <div className='flex items-center'>
+              <Link href='/'>
+                <a className='text-2xl font-bold text-gray-800'>Tanager</a>
+              </Link>
+            </div>
+            <div className='flex items-center'>
+              <button
+                onClick={() => signIn()}
+                className='ml-4 text-gray-800 hover:text-gray-900 hover:underline'
+              >
+                Sign in
+              </button>
+            </div>
+          </nav>
+        </div>
+      </header>
+    );
+  }
 }
--- src/pages/quote_tweets.tsx
+++ src/pages/quote_tweets.tsx
@@ -25,22 +25,19 @@ export default function HomePage() {
   const { data: session } = useSession();
 
   const [userError, setUserError] = React.useState('');
+  const [inputTweetId, setInputTweetId] = React.useState('');
 
   //inputtweetid react state of type Input
   //   const [inputTweetId, setInputTweetId] = React.useState('');
-  const inputTweetId = '';
 
   async function sendRequest(url: any, args: any) {
     console.log('sendRequest url', url);
     console.log('sendRequest args', args);
-    refreshSession();
-    // if (!session || !session.accessToken) {
-    //   console.log('signed out');
-    //   setUserError('Please sign in');
-    //   signOut();
-    // }
+    // refreshSession();
 
     console.log('sendRequest session', session?.accessToken);
+    console.log('sendRequest session', session);
+
     if (args.arg && session && session.accessToken) {
       const fetchQuote = await fetch('/api/twitter/quote', {
         method: 'POST',
@@ -59,15 +56,12 @@ export default function HomePage() {
     }
   }
 
-  //   const { data, error } = useSWRImmutable(
-  //     { url: '/api/twitter/quote', args: inputTweetId },
-  //     sendRequest
-  //   );
-
-  const { data, trigger, error } = useSWRMutation(
+  const { data, trigger, isMutating, error } = useSWRMutation(
     ['/api/twitter/quote', inputTweetId],
     sendRequest
   );
+  console.log('isMutating', isMutating);
+  console.log('error', error);
 
   if (error) {
     if (process.env.NEXT_PUBLIC_VERCEL_ENV != 'production') {
@@ -76,49 +70,51 @@ export default function HomePage() {
   }
 
   console.log('quote swr data', data);
+  // Style page with Tailwind CSS that follows the same design as the rest of the site
+  // Include a input field for the user to enter a tweet id
+  // Show the quote tweet if the user has entered a tweet id and the quote tweet is found and a loading indicator if the quote tweet is being fetched and an error message if the quote tweet is not found
+  if (session) {
+    return (
+      <Layout>
+        <Seo templateTitle='Home' />
+        <Seo />
 
-  return (
-    <Layout>
-      <Seo templateTitle='Home' />
-      <Seo />
+        <main>
+          <section className='bg-white'>
+            <div className='layout flex flex-col items-center justify-center text-center text-black'>
+              <h1 className='mt-8 text-4xl md:text-6xl'>
+                Top liked quote tweets
+              </h1>
+              <p className='mt-4 md:text-lg'>
+                Enter a tweet id to get a quote tweet
+              </p>
 
-      <main>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <h1 className='mt-4'>🐥🐥🐥 Tanager 🐥🐥🐥</h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              Top liked quote tweets{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <>
-                {/* <ButtonLink
-                  className='mt-6'
-                  onClick={() => signOut()}
-                  variant='light'
-                  href={''}
-                >
-                  sign out
-                </ButtonLink> */}
-                {/* input that takes tweet link and fetches all quote tweets */}
-
-                <input
-                  type='text'
-                  placeholder='tweet link'
-                  name='tweet'
-                  //   onChange={(e) => {
-                  //     sendRequest({
-                  //       tweetId: e.target.value,
-                  //       accessToken: session?.accessToken,
-                  //     });
-                  //   }}
-                  onChange={(e) => {
-                    trigger(e.target.value);
-                  }}
-                />
-                {error && (
-                  <p className='mt-2 text-sm text-red-800'>{userError}</p>
-                )}
-
+              <p className='mt-2 text-sm text-gray-700'>
+                <>
+                  <input
+                    type='text'
+                    className='mt-2 rounded-md border border-gray-300 p-2'
+                    placeholder='Enter a tweet id'
+                    value={inputTweetId}
+                    onChange={(e) => setInputTweetId(e.target.value)}
+                  />
+                  <button
+                    className='mt-2 rounded-md border border-gray-300 p-2'
+                    onClick={() => {
+                      trigger(inputTweetId);
+                    }}
+                  >
+                    Get Quote Tweet
+                  </button>
+                </>
+              </p>
+              <p className='mt-2 text-sm text-gray-700'>
+                {isMutating ? 'Loading...' : ''}
+              </p>
+              <p className='mt-2 text-sm text-gray-700'>
+                {userError ? userError : ''}
+              </p>
+              <span className='mt-2 text-sm text-gray-700'>
                 {data && data.data && (
                   <ul>
                     {data.data.map((value: any, index: any) => {
@@ -133,11 +129,39 @@ export default function HomePage() {
                     })}
                   </ul>
                 )}
-              </>
-            </p>
-          </div>
-        </section>
-      </main>
-    </Layout>
-  );
+              </span>
+            </div>
+          </section>
+        </main>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <Seo templateTitle='Home' />
+        <Seo />
+
+        <main>
+          <section className='bg-white'>
+            <div className='layout flex flex-col items-center justify-center text-center text-black'>
+              <h1 className='mt-8 text-4xl md:text-6xl'>
+                Top liked quote tweet
+              </h1>
+              <p className='mt-4 md:text-lg'>You are currently not signed in</p>
+              <p className='mt-2 text-sm text-gray-700'>
+                <>
+                  <button
+                    onClick={() => signIn('twitter')}
+                    className='flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 px-8 py-3 text-base font-medium text-white hover:bg-blue-700 md:py-4 md:px-10 md:text-lg'
+                  >
+                    Sign in with Twitter
+                  </button>
+                </>
+              </p>
+            </div>
+          </section>
+        </main>
+      </Layout>
+    );
+  }
 }
