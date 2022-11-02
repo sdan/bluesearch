a7cbc0c220c25e60be82223a83e4548381d4a579
Surya <git@sdan.io>
2022-11-01 22:57:30 -0400

    feat: add test scripts for prompt engineering

---
+++ src/pages/api/twitter/top_liked/test.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client';
+import { components } from 'twitter-api-sdk/dist/types';
+type Tweet = components['schemas']['Tweet'];
+const prisma = new PrismaClient();
+
+export default async function handle(req: any, res: any) {
+  console.log('in api pull');
+  console.log('req.body', req.body);
+  const { accessToken, twtrId } = req.body;
+  const waitData = (await PullTweets(twtrId)).tweetlist;
+  console.log('waitData data', waitData['TimelineTweets']);
+  // return data
+  for (let i = 0; i < waitData['TimelineTweets'].length; i++) {
+    console.log(
+      waitData['TimelineTweets'][i]['id'] +
+        ': ' +
+        waitData['TimelineTweets'][i]['text']
+    );
+  }
+  res.status(200).json();
+}
+
+export async function PullTweets(twtrId: any) {
+  let tweetlist: any = [];
+
+  console.log("session exists and user's twitter id exists");
+  const prisma = new PrismaClient();
+
+  //find tweets by user id and sort by likes in descending order (most likes first) for the past 24 hours
+  tweetlist = await prisma.account.findFirst({
+    where: {
+      providerAccountId: twtrId,
+    },
+    select: {
+      TimelineTweets: {
+        orderBy: {
+          likes: 'desc',
+        },
+        where: {
+          createdAt: {
+            // Last day
+            gte: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+          },
+        },
+        select: {
+          text: true,
+          id: true,
+        },
+      },
+    },
+  });
+  console.log('twtrId in API', twtrId);
+  console.log('twts', tweetlist[0]);
+
+  //   return {
+  //     tweetlist: JSON.parse(JSON.stringify(tweetlist)),
+  //   };
+  return {
+    tweetlist,
+  };
+}
